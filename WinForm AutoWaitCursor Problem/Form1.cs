6f00ae603840ebdcd492593c1b42b8f14d32b9dd
Robin Luiten <robin.luiten+git@gmail.com>
2015-01-04 16:05:45 +1000

    Example project to show problem on Windows 7 64bit OS's running Visual Studio Community Edition Debug mode and Any CPU platform.

---
+++ WinForm AutoWaitCursor Problem/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace WinForm_AutoWaitCursor_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //AutoWaitCursor.Cursor = Cursors.WaitCursor;
+            //AutoWaitCursor.Delay = new TimeSpan(0, 0, 0, 0, 25);
+            AutoWaitCursor.MainWindowHandle = Handle;
+            AutoWaitCursor.Start();
+        }
+    }
+}
